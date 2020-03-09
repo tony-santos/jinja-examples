e147b172021b6a944cc47be9dc9e39a1784cf4f4
Tony Santos <santos.tony@gmail.com>
2020-03-09 08:48:23 -0400

    added more examples calling the same template

--- code_gen.py
+++ code_gen.py
@@ -11,8 +11,15 @@ env.globals['generate_text'] = env.from_string(template1).module.generate_text
 template_ref = '{{ generate_text(val) }}'
 
 txt1 = env.from_string(template_ref).render(val=4)
-txt2 = env.from_string(template_ref).render(val='this text passed into the template')
+txt2 = env.from_string(template_ref).render(val='this number passed as text ' + '4')
+txt3 = env.from_string(template_ref).render(val='this text passed into the template')
+txt4 = env.from_string(template_ref).render(val='this text passed into the template')
+txt5 = env.from_string(template_ref).render(val='this text passed into the template')
 
 print(f"txt1 = {txt1}")
 print(f"txt2 = {txt2}")
+print(f"txt2 = {txt3}")
+print(f"txt2 = {txt4}")
+print(f"txt2 = {txt5}")
+
 # print(f"type(txt) = {type(txt)}")
